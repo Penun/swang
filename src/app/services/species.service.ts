68ac3224c2168a50fc624a9e0859f480b1a60961
Damon <damon.frankfurth@gmail.com>
2019-08-04 14:38:02 -0600

    Server access options.

--- src/app/services/species.service.ts
+++ src/app/services/species.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -7,26 +8,39 @@ import { UnitService } from './unit.service';
 
 import { Species, SpeAttribute } from '../object-types/species';
 
-import { SPECIES, SPECATTRS } from '../mock-species';
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SpeciesService {
 
     private speciesUrl: string = '/species';
+    private specAttrUrl: string = '/species/attributes';
+
+    private species: Species[];
 
     constructor(
-        private unit: UnitService) { }
+        private unit: UnitService,
+        private http: HttpClient
+    ) { }
 
     getSpecies(): Observable<Species[]> {
         this.unit.log("Spec Serv :: Species Began");
-        return of(SPECIES);
+        return this.http.get<Species[]>(this.speciesUrl).pipe(
+            tap(_ => this.unit.log('Spec Serv :: Species Gotten')),
+            catchError(this.handleError('getSpecies', []))
+        );
     }
 
     getSpecAttr(id: number): Observable<SpeAttribute[]> {
         this.unit.log("Spec Serv :: Spec Began");
-        return of(SPECATTRS.filter(specAttr => specAttr.id === id))
+        return this.http.post<SpeAttribute[]>(this.specAttrUrl, {id: id}, httpOptions).pipe(
+            tap(_ => this.unit.log("Spec Serv :: Spec Gotten")),
+            catchError(this.handleError<SpeAttribute[]>('getSpecAttr'))
+        );
     }
 
     private handleError<T> (operation = 'operation', result?: T) {
